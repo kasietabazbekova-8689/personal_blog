c05c6835a63b11c1d5d06d4385c24fdc0a7960c2
kasietabazbekova-8689 <kasietabazbekova@gmail.com>
2026-07-23 21:23:40 -0500

    vitefile

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/"
-=======
   base: '/',
->>>>>>> 4ac0483 (readme)
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  server: {
-    port: 3000,
-    host: true
-  }
-});
+  plugins: [react()]
+})
